cbbabac7d567bf153f4734457f7e16bee1ce1d96
Peeraphol Chanphol <Peeraphol.c@smm.net>
2026-09-12 00:32:56 +0700

    update accept_create_at

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -75,6 +75,7 @@ export function buildBullConnection() {
   return {
     host: redisUrl.hostname,
     port: Number(redisUrl.port || 6379),
+    username: redisUrl.username || undefined,
     password: redisUrl.password || undefined,
     db: redisUrl.pathname ? Number(redisUrl.pathname.replace("/", "") || 0) : 0,
     maxRetriesPerRequest: null,
